11fa8c2a1207588950642fdc0990faa4b8f67451
emredemir98 <emre.demir20@ogr.sakarya.edu.tr>
2021-12-29 01:26:21 +0300

    update vaccine page

--- lib/vaccine.dart
+++ lib/vaccine.dart
@@ -1,23 +1,21 @@
-import 'package:asi_takip/add_child.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
-import './vaccine.dart';
 final FirebaseAuth _auth = FirebaseAuth.instance;
 
 
-class ChildrenPage extends StatefulWidget{
-  const ChildrenPage({Key? key}) : super(key: key);
+class VaccinePage extends StatefulWidget{
+  const VaccinePage({Key? key}) : super(key: key);
 
   @override
-  _ChildrenPageState createState()=> _ChildrenPageState();
+  _VaccinePageState createState()=> _VaccinePageState();
 }
 
 
-class _ChildrenPageState extends State<ChildrenPage>{
+class _VaccinePageState extends State<VaccinePage>{
   final _fs=  FirebaseFirestore.instance;
 
   Color getMyColor(DateTime date, bool done) {
@@ -77,33 +75,21 @@ class _ChildrenPageState extends State<ChildrenPage>{
                                     '${listOfDocumentSnap[index]['name']+" "+listOfDocumentSnap[index]['surname']}',
                                     style: const TextStyle(fontSize: 24)),
                                 subtitle: Text(
-                                    '${DateFormat('dd-MM-yyyy').format(DateTime.parse((listOfDocumentSnap[index]['birthOfDate']).toDate().toString()))}',
+                                    DateFormat('dd-MM-yyyy').format(DateTime.parse((listOfDocumentSnap[index]['birthOfDate']).toDate().toString())),
                                     style: const TextStyle(fontSize: 16)),
                                 trailing: Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-
-
-                                    IconButton(
-                                      icon: const Icon(Icons.delete),
-                                      onPressed: () async {
-                                        var todoId= listOfDocumentSnap[index].id;
-                                        await listOfDocumentSnap[index]
-                                            .reference
-                                            .delete();
-                                        _usersRef.doc(userId).update({'children':FieldValue.arrayRemove([todoId])});
-                                      },
-                                    ),
-                                    IconButton(
-                                      icon: const Icon(Icons.arrow_forward),
-                                      onPressed: ()  {
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(builder: (context) => AddChildPage()),
-                                        );
-
-                                      },
-                                    ),
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      IconButton(
+                                        icon: const Icon(Icons.delete),
+                                        onPressed: () async {
+                                          var todoId= listOfDocumentSnap[index].id;
+                                          await listOfDocumentSnap[index]
+                                              .reference
+                                              .delete();
+                                          _usersRef.doc(userId).update({'children':FieldValue.arrayRemove([todoId])});
+                                        },
+                                      ),
                                     ]
                                 ),
                               ),
